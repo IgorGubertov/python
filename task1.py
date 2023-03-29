3420e6ab1b938fb71e967f710a3aabdd3aab2f2f
kasim <kasimru@yandex.ru>
2023-03-29 21:25:49 +0300

    Task7_answer

---
+++ task1.py
@@ -0,0 +1,57 @@
+"""
+Задание 1.
+Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init()__),
+который должен принимать данные (список списков) для формирования матрицы.
+[[], [], []]
+Следующий шаг — реализовать перегрузку метода __str()__ для вывода матрицы в привычном виде.
+Далее реализовать перегрузку метода __add()__ для реализации операции
+сложения двух объектов класса Matrix (двух матриц).
+Результатом сложения должна быть новая матрица.
+Подсказка: сложение элементов матриц выполнять поэлементно —
+первый элемент первой строки первой матрицы складываем
+с первым элементом первой строки второй матрицы и т.д.
+Пример:
+1 2 3
+4 5 6
+7 8 9
+1 2 3
+4 5 6
+7 8 9
+Сумма матриц:
+2 4 6
+8 10 12
+14 16 18
+"""
+
+import copy
+
+
+class Matrix:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        my_s = ''
+        for i in range(len(self.matrix)):
+            my_s = my_s + '\t'.join(map(str, self.matrix[i])) + '\n'
+        return my_s
+
+    def __add__(self, other):
+        if len(self.matrix) != len(other.matrix):
+            return None
+        res = copy.deepcopy(self.matrix)
+        for i in range(len(self.matrix)):
+            for k in range(len(self.matrix[i])):
+                res[i][k] = self.matrix[i][k] + other.matrix[i][k]
+        return Matrix(res)
+
+
+l1 = [[1, 2, 4], [3, 4, 5], [5, 6, 6]]
+l2 = [[11, 21, 41], [31, 41, 51], [51, 61, 61]]
+m = Matrix(l1)
+s = Matrix(l2)
+print(m)
+print(s)
+my_z = m + s
+print(f"Сумма матриц: \n{my_z}")
+
