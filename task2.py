5c7003635319e32ed5b98954ca710a06cebd3aab
kasim <kasimru@yandex.ru>
2023-03-29 20:34:16 +0300

    Task7_answer

--- task2.py
+++ task2.py
@@ -1,34 +1,141 @@
 """
 Задание 2.
-Реализовать класс Road (дорога), в котором определить защищенные атрибуты:
-length (длина в метрах), width (ширина в метрах).
-Значения данных атрибутов должны передаваться при создании экземпляра класса.
-Реализовать публичный метод расчета массы асфальта, необходимого для покрытия
-всего дорожного полотна.
-Использовать формулу: длина * ширина * масса асфальта для покрытия одного кв
-метра дороги асфальтом, толщиной в 1 см * число м толщины полотна.
-Массу и толщину сделать публичными атрибутами.
-Проверить работу метода.
-Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
+Реализовать проект расчета суммарного расхода ткани на производство одежды.
+Единственный класс этого проекта — одежда (класс Clothes).
+К типам одежды в этом проекте относятся пальто и костюм.
+У этих типов одежды существуют параметры:
+размер (для пальто) и рост (для костюма). Это могут быть обычные числа: v и h, соответственно.
+Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (v/6.5 + 0.5),
+для костюма (2*h + 0.3). Проверить работу этих методов на реальных данных.
+Реализовать общий подсчет расхода ткани.
+Проверить на практике полученные на этом уроке знания: реализовать
+абстрактный класс для единственного класса проекта,
+проверить на практике работу декоратора @property
+Пример:
+Расход ткани на пальто = 1.27
+Расход ткани на костюм = 20.30
+Общий расход ткани = 21.57
+Два класса: абстрактный и Clothes
 """
 
+from abc import ABC, abstractmethod
 
-class Road:
-    __length = None
-    __width = None
-    weigth = None
-    tickness = None
-
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def intake(self):
-        self.weigth = 25
-        self.tickness = 0.05
-        intake = self.length * self.width * self.weigth * self.tickness / 1000
-        print(f'{intake} т')
+from typing import Any
 
 
-road_to_village = Road(20000, 6)
-road_to_village.intake()
+class AbstractClothes(ABC):
+    """ Интерфейс одежды """
+
+    @property
+    @abstractmethod
+    def tissue_required(self):
+        pass
+
+    @property
+    @abstractmethod
+    def measuring(self):
+        """ Общая размерность одежды """
+        pass
+
+    @abstractmethod
+    def _calc_tissue_required(self):
+        pass
+
+
+class Clothes(AbstractClothes):
+    _clothes = []
+
+    """ Одежда """
+
+    def __init__(self, name: str, measuring: Any):
+        self.name = name
+        self._measuring = measuring
+        self._tissue_required = None
+
+        self._clothes.append(self)
+
+    def _calc_tissue_required(self):
+        raise NotImplemented
+
+    @property
+    def tissue_required(self) -> float:
+        """ Расход ткани """
+        if not self._tissue_required:
+            self._calc_tissue_required()
+
+        return self._tissue_required
+
+    @property
+    def measuring(self) -> Any:
+        """ Узнать размер """
+        return self._measuring
+
+    @measuring.setter
+    def measuring(self, measuring: Any):
+        """ Установить новый размер пальто """
+        self._measuring = measuring
+        self._tissue_required = None
+
+    @property
+    def total_tissue_required(self):
+        """ Ткани на всю одежду """
+        return sum([item.tissue_required for item in self._clothes])
+
+
+class Coat(Clothes):
+    """ Пальтишко """
+
+    def _calc_tissue_required(self):
+        """ посчитать расход ткани для пальто """
+        self._tissue_required = round(self.measuring / 6.5 + 0.5, 2)
+
+    @property
+    def my_v(self) -> Any:
+        """ Узнать размер пальто """
+        return self.measuring
+
+    @my_v.setter
+    def my_v(self, size: Any):
+        """ Установить новый размер пальто """
+        self.measuring = size
+
+    def __str__(self):
+        return f"Для пошива пальто {self.measuring} размера " \
+               f"требуется {self.tissue_required} кв. метров ткани"
+
+
+class Suit(Clothes):
+    """ Костюмчик """
+
+    def _calc_tissue_required(self):
+        """ посчитать расход ткани для костюма """
+        self._tissue_required = round(2 * self.measuring * 0.01 + 0.3, 2)
+
+    @property
+    def my_h(self) -> Any:
+        """ Узнать размер костюма """
+        return self.measuring
+
+    @my_h.setter
+    def my_h(self, height: Any):
+        """ Установить новый размер костюма """
+        self.measuring = height
+
+    def __str__(self):
+        return f"Для пошива костюма на рост {self.measuring} см. " \
+               f"требуется {self.tissue_required} кв. метров ткани"
+
+
+if __name__ == '__main__':
+    coat = Coat('Пальто от Шанель', 5)
+    print(coat)
+    coat.V = 10
+    print(coat)
+
+    suit = Suit('Костюм от Бордо', 178)
+    print(suit)
+    suit.H = 200
+    print(suit)
+
+    print(coat.total_tissue_required)
+    print(suit.total_tissue_required)
--- task3.py
+++ task3.py
@@ -1,95 +1,106 @@
 """
 Задание 3.
-Реализовать базовый класс Worker (работник),
-в котором определить публичные атрибуты name, surname, position (должность),
-и защищенный атрибут income (доход). Последний атрибут должен ссылаться
-на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
-Создать класс Position (должность) на базе класса Worker. В классе Position реализовать публичные методы
-получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
-Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
-проверить значения атрибутов, вызвать методы экземпляров).
-П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
-__str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
+Реализовать программу работы с органическими клетками, состоящими из ячеек.
+Необходимо создать класс Клетка (Cell).
+В его конструкторе инициализировать параметр (quantity),
+соответствующий количеству ячеек клетки (целое число).
+В классе должны быть реализованы методы перегрузки арифметических операторов:
+сложение (add()),
+вычитание (sub()),
+умножение (mul()),
+деление (truediv()).
+Данные методы должны применяться только к клеткам и выполнять увеличение,
+уменьшение, умножение и целочисленное (с округлением до целого) деление клеток, соответственно.
+Сложение. Объединение двух клеток.
+При этом число ячеек общей клетки должно равняться сумме ячеек исходных двух клеток.
+Вычитание. Участвуют две клетки.
+Операцию необходимо выполнять только если разность количества ячеек двух клеток больше нуля,
+иначе выводить соответствующее сообщение.
+Умножение. Создается общая клетка из двух.
+Число ячеек общей клетки определяется как произведение количества ячеек этих двух клеток.
+Деление. Создается общая клетка из двух.
+Число ячеек общей клетки определяется как целочисленное деление количества ячеек этих двух клеток.
+** - По желанию: В классе необходимо реализовать метод make_order(), принимающий экземпляр класса и
+количество ячеек в ряду. Данный метод позволяет организовать ячейки по рядам.
+Метод должен возвращать строку вида *****\n*****\n*****...,
+где количество ячеек между \n равно переданному аргументу.
+Если ячеек на формирование ряда не хватает, то в последний ряд записываются все оставшиеся.
+Например, количество ячеек клетки равняется 12, количество ячеек в ряду — 5.
+Тогда метод make_order() вернет строку: *****\n*****\n**.
+Или, количество ячеек клетки равняется 15, количество ячеек в ряду — 5.
+Тогда метод make_order() вернет строку: *****\n*****\n*****.
+Подсказка: подробный список операторов для перегрузки доступен по ссылке.
+------------------------------------------------------------------------------
+Пример клиентского кода:
+print("Создаем объекты клеток")
+cell1 = Cell(30)
+cell2 = Cell(25)
+cell3 = Cell(10)
+cell4 = Cell(15)
+print()
+print("Складываем")
+print(cell1 + cell2)
+print()
+print("Вычитаем")
+print(cell2 - cell1)
+print(cell4 - cell3)
+print()
+print("Умножаем")
+print(cell2 * cell1)
+print()
+print("Делим")
+print(cell1 / cell2)
+print()
+print("Организация ячеек по рядам")
+print(cell1.make_order(5))
+print(cell2.make_order(10))
+------------------------------------------------------------------------------
+Результаты:
+Создаем объекты клеток
+Складываем
+Сумма клеток = (55)
+Вычитаем
+Разность отрицательна, поэтому операция не выполняется
+Разность клеток = (5)
+Умножаем
+Умножение клеток = (750)
+Делим
+Деление клеток = (1)
+Организация ячеек по рядам
+*****\n *****\n *****\n *****\n *****\n *****\n
+**********\n **********\n *****
 """
 
 
-class Worker:
-    """Класс работника"""
+class Cell:
+    def __init__(self, quantity):
+        self.quantity = int(quantity)
 
-    def __init__(
-            self,
-            name: str,
-            surname: str,
-            position: str,
-            wage: float = 0,
-            bonus: float = 0
-    ):
-        """
-        :param name: Имя работника
-        :param surname: Фамилия работника
-        :param position: Занимаемая должность
-        :param wage: Оклад
-        :param bonus: Премия
-        """
-        self.name = name
-        self.surname = surname
-        self.position = position
+    def __add__(self, other):
+        return f'Две клетки - хорошо, а одна большая - лучше! Размер клетки равен: {self.quantity + other.quantity}'
 
-        self._income = {"wage": wage, "bonus": bonus}
+    def __sub__(self, other):
+        sub = self.quantity - other.quantity
+        return f'Клеточка стала меньше, теперь она равна: {sub} клеточкам' if sub > 0 else 'Клетка исчезла :('
 
+    def __truediv__(self, other):
+        return self.quantity // other.quantity
 
-class Position(Worker):
-    """Класс позиции"""
+    def __mul__(self, other):
+        return self.quantity * other.quantity
 
-    def get_full_name(self, reverse=False):
-        """ Собирает полное имя для позиции в порядке 'заданном reverse
-        :param reverse: порядок следования если False (по умолчанию) 'name surname'
-         если True, то 'surname name'
-        :return: Полное имя
-        """
-        return ' '.join(sorted([self.name, self.surname], reverse=reverse))
-
-    def get_total_income(self):
-        """ Вычисляет полный доход (оклад + премия)
-        :return: Сумма оклада и премии
-        """
-        return sum(self._income.values())
+    def make_order(self, row):
+        result = ''
+        for i in range(int(self.quantity / row)):
+            result += '*' * row + '\n'
+        result += '*' * (self.quantity % row) + '\n'
+        return result
 
 
-if __name__ == '__main__':
-    position_data = [
-        {
-            'name': 'Ivan',
-            'surname': 'Ivanov',
-            'position': 'Scrum master',
-            'wage': 40000,
-            'bonus': 0
-        },
-        {
-            'name': 'Petr',
-            'surname': 'Petrov',
-            'position': 'developer',
-            'wage': 90000,
-            'bonus': 60000
-        },
-        {
-            'name': 'Irina',
-            'surname': 'Ivanova',
-            'position': 'service delivery manager',
-            'wage': 60000,
-            'bonus': 30000
-        },
-    ]
-
-    for item in position_data:
-        position = Position(**item)
-
-        print('#######################################')
-        print('From data: ', item)
-        print('Position name: ', position.name)
-        print('Position surname: ', position.surname)
-        print('Position full name: ', position.get_full_name())
-        print('Position full name reverse: ', position.get_full_name(reverse=True))
-        print('Position position: ', position.position)
-        print('Position total income: ', position.get_total_income())
-        print('#######################################\n\n')
+cell = Cell(24)
+cell_2 = Cell(2)
+print(cell + cell_2)
+print(cell - cell_2)
+print(cell / cell_2)
+print(cell * cell_2)
+print(cell.make_order(7))
